76bb64f83bef22773f548ee1490176e04582c76d
Aakash P <aakashpokkanayil@gmail.com>
2024-10-26 11:00:37 +0530

    Add project files.

---
+++ CltiesService/CltiesService.cs
@@ -0,0 +1,24 @@
+﻿namespace Services
+{
+    public class CltiesService
+    {
+        private List<string> _cities;
+        public CltiesService()
+        {
+            _cities = new List<string>() { 
+                "Kodugallur",
+                "SN puram",
+                "Cochin",
+                "Paravur",
+                "Mathilakam",
+                "Thamarakulam"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+
+    }
+}
